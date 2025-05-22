2e25ca93427581b287826e9f5f6b2a14372dae50
BhuvanChaithanya <99655691+BhuvanChaithanya@users.noreply.github.com>
2025-05-22 12:18:24 +0530

    Time: 0 ms (100%), Space: 17.7 MB (71.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,12 +7,11 @@ class Solution:
 
         i, j = m-1, n-1
 
-        while j >= 0:
+        while j>=0:
             if i>=0 and nums1[i] > nums2[j]:
                 nums1[last] = nums1[i]
-
-                i -= 1
-
+                i -=1
+            
             else:
                 nums1[last] = nums2[j]
                 j -= 1
