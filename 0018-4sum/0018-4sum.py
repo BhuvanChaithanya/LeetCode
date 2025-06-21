bc3e59b3c7ce0569c4aadba67e478092bc0490cc
BhuvanChaithanya <99655691+BhuvanChaithanya@users.noreply.github.com>
2025-06-21 18:01:47 +0530

    Time: 401 ms (40.91%), Space: 18.1 MB (16.26%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,29 +1,29 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
         res = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+            
             for j in range(i+1, len(nums)):
-                if j > i+1 and nums[j] == nums[j-1]:
+                if j>i+1 and nums[j] == nums[j-1]:
                     continue
 
-                left, right = j+1,n-1
-
-                while left < right:
-                    if nums[i]+nums[j]+nums[left]+nums[right] > target:
-                        right -= 1
-                    elif nums[i]+nums[j]+nums[left]+nums[right] < target:
-                        left +=1
-
+                l = j+1
+                r = len(nums)-1
+                while l<r:
+                    tot = nums[i]+nums[j]+nums[l]+nums[r]
+                    if tot == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while l<r and nums[l]== nums[l-1]:
+                            l +=1
+                    elif tot < target:
+                        l +=1
                     else:
-                        res.append([nums[i],nums[j], nums[left], nums[right]])
-
-                        left += 1
-                        right -=1
-                        while left <right and nums[left] == nums[left-1]:
-                            left +=1
+                        r -=1
 
         return res
+
